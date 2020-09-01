56aee08d307023abad22a94b4a3b59cf61299a09
ESRA <esraorhan333@gmail.com>
2020-09-01 11:14:00 +0300

    newTodo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
+import {Model , TodoItem} from './model';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todoApp';
+ 
+ model= new Model();
+ isDisplay=false;
+ doneList:any[]=[];
+
+ getName() {
+   return this.model.user;
+ }
+
+ test(item,i){
+   debugger;
+   item.description=item.description + " Yapıldı";
+   item.action=true;
+   this.doneList.push(item);
+   this.model.items.splice(i,1);
+   console.log(this.doneList);
+ }
+ getItems(){
+   if(this.isDisplay){
+     return this.model.items;
+   }
+   return this.model.items.filter(item=>  !item.action);
+ }
+ addItem(value){
+   if(value !="") {
+     this.model.items.push(new TodoItem(value,false))
+   }
+ }
+ threeDelete(){
+  this.model.items.splice(3,1);
+ }
+
+
 }
---
+++ src/app/model.ts
@@ -0,0 +1,32 @@
+export class Model{
+    user;
+    items;
+
+    constructor(){
+      this.user="Çınar";
+      this.items= [
+                     new TodoItem("Spor", false),
+                     new TodoItem("Kahvaltı", false),
+                     new TodoItem("Kitap Okumak", false),
+                     new TodoItem("Sinema", false),
+                     new TodoItem("Spor", false),
+                     new TodoItem("Kahvaltı", false),
+                     new TodoItem("Kitap Okumak", false),
+                     new TodoItem("Sinema", false),
+                     new TodoItem("Spor", false),
+                     new TodoItem("Kahvaltı", false),
+                     new TodoItem("Kitap Okumak", false),
+                     new TodoItem("Sinema", false)
+                  ];
+    }
+}
+
+export class TodoItem{
+    description;
+    action;
+
+    constructor(description, action) {
+        this.description= description;
+        this.action=action;
+    }
+}
