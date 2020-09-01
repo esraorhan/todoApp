39387be25bb253c3f87e54fe270307189493f74a
safakca <safakakca2012@gmail.com>
2020-09-01 16:09:45 +0300

    yapıldı listesinden görevi geri alma..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Model , TodoItem} from './model';
+import { Model, TodoItem } from './model';
 
 
 @Component({
@@ -9,37 +9,51 @@ import {Model , TodoItem} from './model';
 })
 
 export class AppComponent {
- 
- model= new Model();
- isDisplay=false;
- doneList:any[]=[];
 
- getName() {
-   return this.model.user;
- }
+  model = new Model();
+  isDisplay = false;
+  doneList: any[] = [];
 
- test(item,i){
-   debugger;
-   item.description=item.description + " Yapıldı";
-   item.action=true;
-   this.doneList.push(item);
-   this.model.items.splice(i,1);
-   console.log(this.doneList);
- }
- getItems(){
-   if(this.isDisplay){
-     return this.model.items;
-   }
-   return this.model.items.filter(item=>  !item.action);
- }
- addItem(value){
-   if(value !="") {
-     this.model.items.push(new TodoItem(value,false))
-   }
- }
- threeDelete(){
-  this.model.items.splice(3,1);
- }
+
+  getName() {
+    return this.model.user;
+  }
+
+  test(item, i) {
+    item.description = item.description + " Yapıldı";
+    item.action = true;
+    this.doneList.push(item);
+    this.model.items.splice(i, 1);
+    console.log(this.doneList);
+  }
+
+
+  testUndo(item, i) {
+    debugger;
+    item.action = false;
+    let description = item.description.replace(" Yapıldı", " ");
+    item.description = description;
+    this.model.items.push(item);
+    // this.doneList.shift();
+    this.doneList.splice(i, 1);
+
+
+  }
+
+  getItems() {
+    if (this.isDisplay) {
+      return this.model.items;
+    }
+    return this.model.items.filter(item => !item.action);
+  }
+  addItem(value) {
+    if (value != "") {
+      this.model.items.push(new TodoItem(value, false))
+    }
+  }
+  threeDelete() {
+    this.model.items.splice(3, 1);
+  }
 
 
 }
--- src/app/model.ts
+++ src/app/model.ts
@@ -1,32 +1,32 @@
-export class Model{
+export class Model {
     user;
     items;
 
-    constructor(){
-      this.user="Çınar";
-      this.items= [
-                     new TodoItem("Spor", false),
-                     new TodoItem("Kahvaltı", false),
-                     new TodoItem("Kitap Okumak", false),
-                     new TodoItem("Sinema", false),
-                     new TodoItem("Spor", false),
-                     new TodoItem("Kahvaltı", false),
-                     new TodoItem("Kitap Okumak", false),
-                     new TodoItem("Sinema", false),
-                     new TodoItem("Spor", false),
-                     new TodoItem("Kahvaltı", false),
-                     new TodoItem("Kitap Okumak", false),
-                     new TodoItem("Sinema", false)
-                  ];
+    constructor() {
+        this.user = "Çınar";
+        this.items = [
+            new TodoItem("Spor", false),
+            new TodoItem("Kahvaltı", false),
+            new TodoItem("Kitap Okumak", false),
+            new TodoItem("Sinema", false),
+            new TodoItem("Basketbol", false),
+            new TodoItem("Futbol", false),
+            new TodoItem("Hentbol", false),
+            new TodoItem("Yüzme", false),
+            new TodoItem("Koşu", false),
+            new TodoItem("Yürüyüş", false),
+            new TodoItem("Bisiklet Sürme", false),
+            new TodoItem("Paten", false)
+        ];
     }
 }
 
-export class TodoItem{
+export class TodoItem {
     description;
     action;
 
     constructor(description, action) {
-        this.description= description;
-        this.action=action;
+        this.description = description;
+        this.action = action;
     }
 }
